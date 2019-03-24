57015b26c27dd0beeddb7c71ce97427c01f87ae6
Lee875083146 <vbof53@163.com>
2019-03-24 16:19:48 +0800

    Initial commit

---
+++ src/main/java/com/nopainanymore/inspired/InspiredApplication.java
@@ -0,0 +1,13 @@
+package com.nopainanymore.inspired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspiredApplication.class, args);
+    }
+
+}
